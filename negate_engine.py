0e980361faf48eb2233722854fb0a198b11de680
bobby robidoux <robidouxrd@gmail.com>
2025-04-16 13:28:30 -0700

    ollama/req

--- negate_engine.py
+++ negate_engine.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+import ollama
 
 
 
@@ -7,10 +8,20 @@ def main():
     dataset = load_dataset("orionweller/NevIR")
 
     train_data = dataset["train"]
-    print(train_data[0]["q1"])  # Print the first example from the training split
-    print(train_data[0]["q2"])  # Print the first example from the training split
-    print(train_data[0]["doc1"])  # Print the first example from the training split
-    print(train_data[0]["doc2"])  # Print the first example from the training split
+    # print(train_data[0]["q1"])  # Print the first example from the training split
+    # print(train_data[0]["q2"])  # Print the first example from the training split
+    # print(train_data[0]["doc1"])  # Print the first example from the training split
+    # print(train_data[0]["doc2"])  # Print the first example from the training split
+
+
+    client = ollama.Client()
+
+    model = "gemma3"
+    prompt = "hows it going?"
+
+    response = client.generate(model=model, prompt=prompt)
+
+    print(response["response"])
 
 
 
